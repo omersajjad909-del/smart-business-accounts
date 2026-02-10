2f4c8890269f1fdb5a42bc1cc7b5fe6b8fddcd0b
omersajjad909-del <omersajjad909@gmail.com>
2026-02-10 21:27:41 +0500

    update api routes, dashboard pages, and schema

--- components/GlobalSearch.tsx
+++ components/GlobalSearch.tsx
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿"use client";
+﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿"use client";
 
 import { useState, useEffect, useRef } from "react";
 import { useRouter, useSearchParams, usePathname } from "next/navigation";
