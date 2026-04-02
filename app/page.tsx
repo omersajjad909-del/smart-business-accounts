ad23273d832b5307a6258f6153b438491cc6d465
Umer-Sajjad <umersajjad981@gmail.com>
2026-04-03 00:54:51 +0300

    make root domain open landing directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-import { redirect } from "next/navigation";
+import Hero from "./(marketing)/landing/components/Hero";
+import SolutionSection from "./(marketing)/landing/components/SolutionSection";
+import FeaturesSection from "./(marketing)/landing/components/FeaturesSection";
+import ModulesSection from "./(marketing)/landing/components/ModulesSection";
+import PricingSection from "./(marketing)/landing/components/Pricing";
+import TestimonialsSection from "./(marketing)/landing/components/Testimonials";
+import FAQSection from "./(marketing)/landing/components/FAQSection";
+import CTASection from "./(marketing)/landing/components/CTASection";
+import NewsletterSection from "./(marketing)/landing/components/NewsletterSection";
+import CookieBanner from "./(marketing)/landing/components/CookieBanner";
 
 export default function RootPage() {
-  redirect("/landing");
+  return (
+    <>
+      <Hero />
+      <SolutionSection />
+      <FeaturesSection />
+      <ModulesSection />
+      <TestimonialsSection />
+      <PricingSection />
+      <FAQSection />
+      <NewsletterSection />
+      <CTASection />
+      <CookieBanner />
+    </>
+  );
 }
