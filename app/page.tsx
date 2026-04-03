4480c88bf45b51d45ccbee061679f7fe1d5c5686
Umer-Sajjad <umersajjad981@gmail.com>
2026-04-03 12:55:25 +0300

    restore marketing shell on root homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import Offer from "./(marketing)/landing/components/Offer";
+import Navbar from "./(marketing)/landing/components/navbar";
 import Hero from "./(marketing)/landing/components/Hero";
 import SolutionSection from "./(marketing)/landing/components/SolutionSection";
 import FeaturesSection from "./(marketing)/landing/components/FeaturesSection";
@@ -10,6 +12,8 @@ import FAQSection from "./(marketing)/landing/components/FAQSection";
 import CTASection from "./(marketing)/landing/components/CTASection";
 import NewsletterSection from "./(marketing)/landing/components/NewsletterSection";
 import CookieBanner from "./(marketing)/landing/components/CookieBanner";
+import Footer from "./(marketing)/landing/components/Footer";
+import ChatWidget from "./(marketing)/landing/components/ChatWidget";
 
 async function getRequestHost() {
   const headerStore = await headers();
@@ -28,17 +32,25 @@ export default async function RootPage() {
   }
 
   return (
-    <>
-      <Hero />
-      <SolutionSection />
-      <FeaturesSection />
-      <ModulesSection />
-      <TestimonialsSection />
-      <PricingSection />
-      <FAQSection />
-      <NewsletterSection />
-      <CTASection />
-      <CookieBanner />
-    </>
+    <div className="flex min-h-screen flex-col bg-[#060919]">
+      <Offer />
+      <div className="sticky top-0 z-50">
+        <Navbar />
+      </div>
+      <main className="grow overflow-x-hidden">
+        <Hero />
+        <SolutionSection />
+        <FeaturesSection />
+        <ModulesSection />
+        <TestimonialsSection />
+        <PricingSection />
+        <FAQSection />
+        <NewsletterSection />
+        <CTASection />
+        <CookieBanner />
+      </main>
+      <Footer />
+      <ChatWidget />
+    </div>
   );
 }
