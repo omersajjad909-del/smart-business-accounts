7e224ad989c61fd471df712d3f3c0bd9dad323ab
Umer-Sajjad <umersajjad981@gmail.com>
2026-08-15 13:03:03 +0500

    update api routes, dashboard pages, and schema

---
+++ tmp-reset-admin.js
@@ -0,0 +1,29 @@
+﻿const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+(async () => {
+  const prisma = new PrismaClient();
+  const email = "finovaos.app@gmail.com";
+  const password = "12345678";
+  const hash = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (user) {
+    await prisma.user.update({ where: { id: user.id }, data: { password: hash } });
+    console.log("USER_UPDATED");
+  }
+
+  const adminUser = await prisma.adminUser.findUnique({ where: { email } });
+  if (adminUser) {
+    await prisma.adminUser.update({ where: { id: adminUser.id }, data: { passwordHash: hash } });
+    console.log("ADMINUSER_UPDATED");
+  }
+
+  if (!user && !adminUser) {
+    console.log("NO_MATCH");
+  }
+
+  await prisma.$disconnect();
+})().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
